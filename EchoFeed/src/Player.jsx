50ac2eb297ab32308be17a77d5ae910b9c4c2c70
terwo <105677497+terwo@users.noreply.github.com>
2023-11-19 16:49:35 -0800

    Forgot to commit, try to implement default state

--- EchoFeed/src/Player.jsx
+++ EchoFeed/src/Player.jsx
@@ -2,28 +2,49 @@ import React from 'react'
 import { useState } from "react"
 import './App.css'
 import './Player.css'
+import microphone from './assets/microphone.png'
 
 function Player(props) {
     // where title is headline title, image taken from news, summary from AgentHub, link to full page, reader as speech to text
     const { title, category, summary, linkFull} = props;
 
+    let isDefaultState = true;
+
+    const  defaultText= "Click on an article to view and play audio";
+    const defaultImage = microphone;
+
     return (
         <>
-        <div className="player-container">
-            <div className="player-category">
-                <h3>{category}</h3>
+        
+        {isDefaultState && (
+            <div className="player-container">
+                <div className="default-image-container">
+                    {/* Render the image only if it's the default state */}
+                    <img src={defaultImage} alt="Default Image" />
+                </div>
+                <div className="default-text-container">
+                    <p>{defaultText}</p>
+                </div>
             </div>
-            <div className = "player-title">
-                <h1>{title}</h1>
+        )}
+        
+        {!isDefaultState && (
+            <div className="player-container">
+                <div className="player-category">
+                    <h3>{category}</h3>
+                </div>
+                <div className="player-title">
+                    <h1>{title}</h1>
+                </div>
+                <div className="player-summary">
+                    <p>{summary}</p>
+                </div>
+                <div className="player-reader">
+                    {/* Your reader button or any other content specific to the Player */}
+                    <button className="news-button">Reader</button>
+                </div>
             </div>
-            <div className="content-container">
-                <p className="news-summary">{summary}</p>
-                <a href={linkFull} className="news-link">View full</a>
-            </div>
-            <div className= "player-reader">
-                <button className="news-button">Reader</button>
-            </div>
-        </div>
+        )}
         </>
     )
 }
