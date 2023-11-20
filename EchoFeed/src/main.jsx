ade4bc7e4cb9505426d00150c4df82c06da97e38
Robbie Laughlen <robbity@students.cs.ubc.ca>
2023-11-19 18:36:15 -0800

    New change

--- EchoFeed/src/main.jsx
+++ EchoFeed/src/main.jsx
@@ -6,12 +6,14 @@ import Onboarding from './Onboarding.jsx'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/onboarding" element={<Onboarding/>}/>
+      <Route path="/home" element={<App/>}/>
     </Routes>
   </BrowserRouter>
 )
